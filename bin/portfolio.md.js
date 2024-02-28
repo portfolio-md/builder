f0d5acb90db1474ce4d03529a042a1018515470c
dmytrobaida <dmytrobaida@outlook.com>
2024-02-28 17:36:37 +0200

    adds copying dist to root folder

--- bin/portfolio.md.js
+++ bin/portfolio.md.js
@@ -2,11 +2,34 @@
 
 const { spawn } = require('child_process');
 const { argv } = require('process');
+const fs = require('fs');
+
+// TODO: Validate this
+const command = argv[2];
+const srcDir = 'node_modules/@portfolio.md/builder/dist/engine/out';
+const destDir = './dist';
 
 spawn(
   'yarn',
-  ['next', argv[2], 'node_modules/@portfolio.md/builder/dist/engine'],
+  ['next', command, 'node_modules/@portfolio.md/builder/dist/engine'],
   {
     stdio: 'inherit',
   }
-);
+).on('close', () => {
+  if (command !== 'build') {
+    return;
+  }
+
+  if (fs.existsSync(srcDir)) {
+    // clean destination dir before copying
+    if (fs.existsSync(destDir)) {
+      fs.rmSync(destDir, {
+        recursive: true,
+      });
+    }
+
+    fs.cpSync(srcDir, destDir, {
+      recursive: true,
+    });
+  }
+});
