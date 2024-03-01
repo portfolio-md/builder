0e3a4e3bbd416cb117919e8d1f0662457c91eb0f
dmytrobaida <dmytrobaida@outlook.com>
2024-03-01 18:14:20 +0200

    updates ui lib

---
+++ engine/src/app/[slug]/layout.tsx
@@ -0,0 +1,39 @@
+import { getMenu } from '../../utils/common';
+import { ConfigService } from '../../utils/config-provider';
+
+export default async function RootLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { slug: string };
+}) {
+  const { config, template, images } = await ConfigService.getConfig();
+  const { home, socials, credentials, pages } = config;
+  const menu = getMenu(pages);
+  const page = pages[params.slug];
+
+  return template.render({
+    header: {
+      home: {
+        title: home.title,
+        url: home.url,
+        logo: images[home.logoName].dataUri,
+      },
+      menu: menu,
+      download: page.genPdf
+        ? {
+            title: 'PDF',
+            url: `/pdf/${params.slug}.pdf`,
+          }
+        : undefined,
+    },
+    content: {
+      children: children,
+    },
+    footer: {
+      socials: socials,
+      credentials: credentials,
+    },
+  });
+}
--- engine/src/app/[slug]/page.tsx
+++ engine/src/app/[slug]/page.tsx
@@ -1,4 +1,3 @@
-import { getMenu } from '../../utils/common';
 import mdxRenderer from '../../utils/mdx-renderer';
 import { ConfigService } from '../../utils/config-provider';
 
@@ -12,32 +11,5 @@ export async function generateStaticParams() {
 }
 
 export default async function Page({ params }: { params: { slug: string } }) {
-  const { config, template, images } = await ConfigService.getConfig();
-  const { home, socials, credentials, pages } = config;
-  const menu = getMenu(pages);
-  const page = pages[params.slug];
-
-  return template.render({
-    header: {
-      home: {
-        title: home.title,
-        url: home.url,
-        logo: images[home.logoName].dataUri,
-      },
-      menu: menu,
-      download: page.genPdf
-        ? {
-            title: 'PDF',
-            url: `/pdf/${params.slug}.pdf`,
-          }
-        : undefined,
-    },
-    content: {
-      children: await mdxRenderer(params.slug),
-    },
-    footer: {
-      socials: socials,
-      credentials: credentials,
-    },
-  });
+  return await mdxRenderer(params.slug);
 }
